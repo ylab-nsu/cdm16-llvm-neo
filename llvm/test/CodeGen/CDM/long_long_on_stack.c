dc47307a00361c0fe62c64aaf692dc44ccba5279
Proletcultist <zenin2606@gmail.com>
2025-11-18 12:45:51 +0700

    Fixed lit test for long long on stack

--- llvm/test/CodeGen/CDM/long_long_on_stack.c
+++ llvm/test/CodeGen/CDM/long_long_on_stack.c
@@ -1,35 +1,15 @@
 // RUN: clang -target cdm -O0 -S -emit-llvm -o /dev/stdout %s | llc -mtriple=cdm | FileCheck %s
 
-__attribute__((noinline))
-long long foo(long long a, long long b){
-	return a + b;
-}
-
-long long bar(){
-// CHECK-LABEL: bar>
-// CHECK-NEXT: # %bb.0:
-// CHECK-NEXT: push fp
-// CHECK-NEXT: ldsp fp
-// CHECK-NEXT: addsp -16
-// CHECK-NEXT: ldsp r0
-// CHECK-NEXT: ldi r2, 14
-// CHECK-NEXT: ldi r1, 0
-// CHECK-NEXT: stw r0, r2, r1
-// CHECK-NEXT: ldi r2, 12
-// CHECK-NEXT: stw r0, r2, r1
-// CHECK-NEXT: ldi r2, 10
-// CHECK-NEXT: ldi r3, 1883
-// CHECK-NEXT: stw r0, r2, r3
-// CHECK-NEXT: ldi r2, 8
-// CHECK-NEXT: ldi r3, -13035
-// CHECK-NEXT: stw r0, r2, r3
-// CHECK-NEXT: ldi r0, 2
-// CHECK-NEXT: move r1, r2
-// CHECK-NEXT: move r1, r3
-// CHECK-NEXT: jsr foo
-// CHECK-NEXT: addsp 16
-// CHECK-NEXT: stsp fp
-// CHECK-NEXT: pop fp
-// CHECK-NEXT: rts
-	return foo(2, 123456789);
+long long foo(long long value) {
+// CHECK-LABEL: foo>
+// CHECK: ldi r4, -12
+// CHECK-NEXT: add r4, fp, r4
+// CHECK-NEXT: ldi r5, 6
+// CHECK-NEXT: stw r4, r5, r3
+// CHECK-NEXT: ldi r3, 4
+// CHECK-NEXT: stw r4, r3, r2
+// CHECK-NEXT: ldi r2, 2
+// CHECK-NEXT: stw r4, r2, r1
+// CHECK-NEXT: ssw r0, -12
+    return value;
 }
