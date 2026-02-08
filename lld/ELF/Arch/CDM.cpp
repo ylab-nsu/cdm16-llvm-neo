c58c39ec6f9160dd4fed4fa37a5de818c590e7c8
aelsi2 <eliseevandrey2005@gmail.com>
2026-02-08 11:51:59 +0700

    Format CDM TargetInfo in LLD

--- lld/ELF/Arch/CDM.cpp
+++ lld/ELF/Arch/CDM.cpp
@@ -17,56 +17,50 @@ using namespace llvm::object;
 
 namespace {
 
-class CDM final: public TargetInfo {
+class CDM final : public TargetInfo {
 public:
-    CDM(Ctx &ctx);
-    RelExpr getRelExpr(RelType type, const Symbol &s,
+  CDM(Ctx &ctx);
+  RelExpr getRelExpr(RelType type, const Symbol &s,
                      const uint8_t *loc) const override;
-    void relocate(uint8_t *loc, const Relocation &rel,
+  void relocate(uint8_t *loc, const Relocation &rel,
                 uint64_t val) const override;
-    int64_t getImplicitAddend(const uint8_t *buf,
-                RelType type) const override;
+  int64_t getImplicitAddend(const uint8_t *buf, RelType type) const override;
 };
-}
+} // namespace
 
-CDM::CDM(Ctx &ctx): TargetInfo(ctx) {
-    defaultImageBase = 0x00000;
-}
+CDM::CDM(Ctx &ctx) : TargetInfo(ctx) { defaultImageBase = 0x00000; }
 
 RelExpr CDM::getRelExpr(RelType type, const Symbol &s,
-                     const uint8_t *loc) const {
-    switch (type) {
-    case R_CDM_16:
-        return R_ABS;
-    default:
-        Err(ctx) << getErrorLoc(ctx, loc) << "unknown relocation (" << type.v
-                << ") against symbol " << &s;
+                        const uint8_t *loc) const {
+  switch (type) {
+  case R_CDM_16:
+    return R_ABS;
+  default:
+    Err(ctx) << getErrorLoc(ctx, loc) << "unknown relocation (" << type.v
+             << ") against symbol " << &s;
     return R_NONE;
-    }
+  }
 }
 
-void CDM::relocate(uint8_t *loc, const Relocation &rel,
-                uint64_t val) const {
-    switch (rel.type) {
-    case R_CDM_16:
-        checkUInt(ctx, loc, val, 16, rel);
-        write16le(loc, val);
-        break;
-    default:
-        llvm_unreachable("unknown relocation");
-    }
+void CDM::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
+  switch (rel.type) {
+  case R_CDM_16:
+    checkUInt(ctx, loc, val, 16, rel);
+    write16le(loc, val);
+    break;
+  default:
+    llvm_unreachable("unknown relocation");
+  }
 }
 
-int64_t CDM::getImplicitAddend(const uint8_t *buf,
-                RelType type) const {
-    switch (type) {
-    case R_CDM_16:
-        return SignExtend64<16>(read16le(buf));
-    default:
-        InternalErr(ctx, buf) << "cannot read addend for relocation " << type;
-        return 0;
-    }
+int64_t CDM::getImplicitAddend(const uint8_t *buf, RelType type) const {
+  switch (type) {
+  case R_CDM_16:
+    return SignExtend64<16>(read16le(buf));
+  default:
+    InternalErr(ctx, buf) << "cannot read addend for relocation " << type;
+    return 0;
+  }
 }
 
 void elf::setCDMTargetInfo(Ctx &ctx) { ctx.target.reset(new CDM(ctx)); }
-
