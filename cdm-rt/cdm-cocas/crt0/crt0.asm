12e6c1ede83dda74b332d9eca56675918446d9ca
Proletcultist <zenin2606@gmail.com>
2026-02-14 15:42:30 +0700

    Renamed symbols in cocas crt0

--- cdm-rt/cdm-cocas/crt0/crt0.asm
+++ cdm-rt/cdm-cocas/crt0/crt0.asm
@@ -1,33 +1,30 @@
 asect 0x0
-_start, _default_handler1, _default_handler2, _default_handler3, _default_handler4: ext
+_start, _cdm_default_handler_1, _cdm_default_handler_2, _cdm_default_handler_3, _cdm_default_handler_4: ext
 
 dc _start, 0
-dc _default_handler1, 0   # Unaligned SP
-dc _default_handler2, 0   # Unaligned PC
-dc _default_handler3, 0   # Invalid instruction
-dc _default_handler4, 0   # Double fault
-align 0x80
+dc _cdm_default_handler_1, 0   # Unaligned SP
+dc _cdm_default_handler_2, 0   # Unaligned PC
+dc _cdm_default_handler_3, 0   # Invalid instruction
+dc _cdm_default_handler_4, 0   # Double fault
 
-rsect default_handler
-
-_default_handler1>
-  ldi r0, 0xDED1
-  br _default_handlers_end
-_default_handler2>
-  ldi r0, 0xDED2
-  br _default_handlers_end
-_default_handler3>
-  ldi r0, 0xDED3
-  br _default_handlers_end
-_default_handler4>
-  ldi r0, 0xDED4
-_default_handlers_end:
-  pop r1                  # Pop saved PC into r1
-  halt
-
-rsect start
+rsect _cdm_crt0
 
 main: ext
+
+_cdm_default_handler_1>
+  ldi r0, 0xDED1
+  br _cdm_default_handlers_end
+_cdm_default_handler_2>
+  ldi r0, 0xDED2
+  br _cdm_default_handlers_end
+_cdm_default_handler_3>
+  ldi r0, 0xDED3
+  br _cdm_default_handlers_end
+_cdm_default_handler_4>
+  ldi r0, 0xDED4
+_cdm_default_handlers_end:
+  pop r1                  # Pop saved PC into r1
+  halt
 
 _start>
   jsr main
