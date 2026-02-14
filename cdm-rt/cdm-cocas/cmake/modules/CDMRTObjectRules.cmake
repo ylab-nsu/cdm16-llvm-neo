d7d2aefc98d8a4f32a347b931812c3f2e22c068c
Proletcultist <zenin2606@gmail.com>
2026-02-14 15:43:31 +0700

    Changed cmake configs

--- cdm-rt/cdm-cocas/cmake/modules/CDMRTObjectRules.cmake
+++ cdm-rt/cdm-cocas/cmake/modules/CDMRTObjectRules.cmake
@@ -10,9 +10,12 @@ function(create_object_library target output)
     ${ARGN}
   )
 
+  add_library(${target}-objects OBJECT ${CREATE_OBJECT_LIBRARY_SRCS})
+  target_compile_options(${target}-objects PRIVATE -target cdm-cocas)
+
   add_custom_command(OUTPUT ${output}
-      COMMAND   ${CMAKE_COMMAND} -E env "COCAS=${COCAS_EXECUTABLE}"
-                ${CMAKE_C_COMPILER} -target cdm-cocas -r -o ${output} ${CREATE_OBJECT_LIBRARY_SRCS})
+                     COMMAND ${COCAS_EXECUTABLE} -m -o ${output} $<TARGET_OBJECTS:${target}-objects>
+                     DEPENDS ${target}-objects)
   add_custom_target(${target} ALL DEPENDS ${output})
 
 endfunction(create_object_library)
