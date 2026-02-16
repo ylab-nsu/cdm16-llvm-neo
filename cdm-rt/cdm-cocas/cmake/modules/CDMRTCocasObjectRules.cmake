25859fa1f3db37f587ca1602160ce224fb070988
Proletcultist <zenin2606@gmail.com>
2026-02-16 23:17:16 +0700

    Added possibility to pass path to coccas to cmake
    You can specify path to cocas, which will be used for building with
    `-DCOCAS_EXECUTABLE=<path>` option

--- cdm-rt/cdm-cocas/cmake/modules/CDMRTCocasObjectRules.cmake
+++ cdm-rt/cdm-cocas/cmake/modules/CDMRTCocasObjectRules.cmake
@@ -10,12 +10,9 @@ function(create_object_library target output)
     ${ARGN}
   )
 
-  add_library(${target}-objects OBJECT ${CREATE_OBJECT_LIBRARY_SRCS})
-  target_compile_options(${target}-objects PRIVATE -target cdm-cocas)
-
   add_custom_command(OUTPUT ${output}
-                     COMMAND ${COCAS_EXECUTABLE} -m -o ${output} $<TARGET_OBJECTS:${target}-objects>
-                     DEPENDS ${target}-objects)
+                     COMMAND ${CMAKE_COMMAND} -E env COCAS="${COCAS_EXECUTABLE}"
+                     ${CMAKE_ASM_COMPILER} -target cdm-cocas -r -o ${output} ${CREATE_OBJECT_LIBRARY_SRCS})
   add_custom_target(${target} ALL DEPENDS ${output})
 
 endfunction(create_object_library)
