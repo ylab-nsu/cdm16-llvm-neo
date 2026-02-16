69881705fbb388d7fd748972111a7cb4878cd5fb
Proletcultist <zenin2606@gmail.com>
2026-02-16 23:23:28 +0700

    Rewrited structs in end-to-end testing system
    Used dataclasses instead of silly namedtuples

--- llvm/test_cdm/src/lib/structs.py
+++ llvm/test_cdm/src/lib/structs.py
@@ -1,11 +1,39 @@
-from collections import namedtuple
+from pathlib import Path
+from dataclasses import dataclass
 
-BaseTest = namedtuple('Test', ['name', 'files', 'assertions'])
-BaseTest.__str__ = lambda self : f'\033[1m{self.name}:\033[0m\n\tFiles:\n\t\t{'\n\t\t'.join(map(str, self.files))}\n\tAssertions:\n\t\t{'\n\t\t'.join(map(str, self.assertions))}'
+@dataclass
+class RegisterAssertion:
+    reg: str
+    value: int
 
-RegisterAssertion = namedtuple('RegisterAssertion', ['reg', 'value'])
-AbsoluteSectionAssertion = namedtuple('AbsoluteSectionAssertion', ['symbol', 'content'])
-MemoryAssertion = namedtuple('MemoryAssertion', ['address', 'content'])
+@dataclass
+class AbsoluteSectionAssertion:
+    symbol: str
+    content: list[int]
 
-ProcessorInfo = namedtuple('ProcessorInfo', ['regs', 'regsizes', 'memsize'])
-ProcessorInfo.__str__ = lambda self : f'\033[1mProcessor:\033[0m\n\tRegs: {' '.join(self.regs)}\n\tRegsizes: {' '.join(map(str, self.regsizes))}\n\tMemsize: {self.memsize}'
+@dataclass
+class MemoryAssertion:
+    address: int
+    content: list[int]
+
+@dataclass
+class BaseTest:
+    name: str
+    files: list[Path]
+    assertions: list[RegisterAssertion | AbsoluteSectionAssertion | MemoryAssertion]
+
+    def __str__(self):
+        files_string = '\n\t\t'.join(map(str, self.files))
+        assertions_string = '\n\t\t'.join(map(str, self.assertions))
+        return f'\033[1m{self.name}:\033[0m\n\tFiles:\n\t\t{files_string}\n\tAssertions:\n\t\t{assertions_string}'
+
+@dataclass
+class ProcessorInfo:
+    regs: list[str]
+    regsizes: list[int]
+    memsize: int
+
+    def __str__(self):
+        regs_string = ' '.join(self.regs)
+        regsizes_string = ' '.join(map(str, self.regsizes))
+        return f"\033[1mProcessor:\033[0m\n\tRegs: {regs_string}\n\tRegsizes: {regsizes_string}\n\tMemsize: {self.memsize}"
