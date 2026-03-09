080d94960bae6477d0d32fdead950f4d9b42e75c
Proletcultist <zenin2606@gmail.com>
2026-03-09 23:42:07 +0700

    Fix tests_runner and producers in testing system

--- llvm/test_cdm/src/testing_system/tests_runner.py
+++ llvm/test_cdm/src/testing_system/tests_runner.py
@@ -49,20 +49,20 @@ def run_tests(connection: CocoemuConnection, config: Configuration, test_cases: 
   with io.StringIO() as errors_stream:
     try:
       for case in test_cases:
-        for result in case.run(connection, errors_stream):
-          if result:
-            total_succ+=1
-            if config.log:
-              print(f'[\033[32mSuccess\033[0m] {case.name}')
-            else:
-              print('\033[32m.\033[0m', end="", flush=True)
+        result = case.run(connection, errors_stream)
+        if result:
+          total_succ+=1
+          if config.log:
+            print(f'[\033[32mSuccess\033[0m] {case.name}')
           else:
-            total_fails+=1
-            if config.log:
-              print(errors_stream.getvalue(), end="")
-              errors_stream.truncate(0)
-            else:
-              print('\033[31mF\033[0m', end="", flush=True)
+            print('\033[32m.\033[0m', end="", flush=True)
+        else:
+          total_fails+=1
+          if config.log:
+            print(errors_stream.getvalue(), end="")
+            errors_stream.truncate(0)
+          else:
+            print('\033[31mF\033[0m', end="", flush=True)
     finally:
       print("\n", errors_stream.getvalue(), sep = "")
       print(f'Passed {total_succ} out of {total_succ + total_fails}')
