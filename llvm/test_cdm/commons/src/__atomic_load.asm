40bf7b3231a8ce4ccdd81a3cf46e67c87a0a2e21
NetscapeNav <netscapenavigator@yahoo.com>
2026-03-17 09:45:02 +0700

    Fix code review: RSECT prefix, push/addsp, not instruction, udivmoddi4

--- llvm/test_cdm/commons/src/__atomic_load.asm
+++ llvm/test_cdm/commons/src/__atomic_load.asm
@@ -5,8 +5,16 @@ __atomic_load>
     push r5
     ldps r4
     di
+__atomic_load_loop:
+    tst r0
+    bz __atomic_load_done
     ldw r1, r5
     stw r2, r5
+    add r1, 2
+    add r2, 2
+    sub r0, 2
+    br __atomic_load_loop
+__atomic_load_done:
     stps r4
     pop r5
     pop r4
--- llvm/test_cdm/commons/src/__atomic_store.asm
+++ llvm/test_cdm/commons/src/__atomic_store.asm
@@ -5,8 +5,16 @@ __atomic_store>
     push r5
     ldps r4
     di
+__atomic_store_loop:
+    tst r0
+    bz __atomic_store_done
     ldw r2, r5
     stw r1, r5
+    add r1, 2
+    add r2, 2
+    sub r0, 2
+    br __atomic_store_loop
+__atomic_store_done:
     stps r4
     pop r5
     pop r4
