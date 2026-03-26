42dcbb64e040498b4ffc711c94a3649ac687e281
Proletcultist <zenin2606@gmail.com>
2026-03-26 08:51:22 +0700

    Add testing system configutation verification

--- llvm/utils/test_cdm/test_cdm/testing_system/configuration.py
+++ llvm/utils/test_cdm/test_cdm/testing_system/configuration.py
@@ -1,5 +1,12 @@
+import itertools
 from dataclasses import dataclass, field
 from pathlib import Path
+
+@dataclass
+class InvalidConfigurationError(Exception):
+  message: str
+  def __str__(self) -> str:
+    return self.message
 
 @dataclass
 class Configuration:
@@ -12,3 +19,8 @@ class Configuration:
   log: bool = False
   search_points: list[Path] = field(default_factory=list)
   include_paths: list[Path] = field(default_factory=list)
+
+  def verify(self) -> None:
+    for path in itertools.chain([self.clang_path, self.cocas_path, self.cocoemu_path, self.resources_path], self.include_paths):
+      if not path.exists():
+        raise InvalidConfigurationError(f'File or directory "{str(path)}" doesn\'t exist')
