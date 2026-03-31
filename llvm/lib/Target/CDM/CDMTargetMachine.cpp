2d8351a278ac08563b13beaf650871940f5faa85
aelsi2 <eliseevandrey2005@gmail.com>
2026-03-31 18:49:47 +0700

    Use ELF mangling for CDM
    This commit removes the custom "CDM" mangling scheme that uses "__L" as
    the local label prefix and uses ELF mangling with ".L" prefix instead.
    
    We don't need custom mangling anymore becuase Cocas now supports '.' at
    the start of label names. And in AsmInfo we also have '.L'

--- llvm/lib/Target/CDM/CDMTargetMachine.cpp
+++ llvm/lib/Target/CDM/CDMTargetMachine.cpp
@@ -19,8 +19,8 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeCDMTarget() {
 static std::string computeDataLayout() {
   // XXX Build the triple from the arguments.
   // This is hard-coded for now for this example target.
-  return "e-S16-p:16:16-i8:8-i16:16-i32:16-i64:16-f16:16-f32:16-f64:16-f128:16-"
-         "m:C-n16";
+  return "e-m:e-p:16:16-i8:8-i16:16-i32:16-i64:16-f16:16-f32:16-f64:16-f128:16-"
+         "n16-S16";
 }
 
 CDMTargetMachine::CDMTargetMachine(const Target &T, const Triple &TT,
--- llvm/test/CodeGen/CDM/inst_aliases.ll
+++ llvm/test/CodeGen/CDM/inst_aliases.ll
@@ -1,4 +1,4 @@
-target datalayout = "e-S16-p:16:16-i8:8-i16:16-i32:16-i64:16-f16:16-f32:16-f64:16-f128:16-m:C-n16"
+target datalayout = "e-m:e-p:16:16-i8:8-i16:16-i32:16-i64:16-f16:16-f32:16-f64:16-f128:16-n16-S16"
 
 ; RUN: llc -mtriple=cdm-cocas < %s | FileCheck %s
 
