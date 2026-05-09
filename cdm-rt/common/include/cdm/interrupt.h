465e39eb7b7dfb2dfee8b535f5499c138f5c5948
aelsi2 <eliseevandrey2005@gmail.com>
2026-05-09 23:31:45 +0700

    Add various improvements for <cdm/*.h>

--- cdm-rt/common/include/cdm/interrupt.h
+++ cdm-rt/common/include/cdm/interrupt.h
@@ -1,7 +1,8 @@
 #if defined(__CDM__) && !defined(_CDM_RT_INTERRUPT_H)
 #define _CDM_RT_INTERRUPT_H
 
-// Enables all interrupts.
+// Enable hardware interrupts.
+__attribute__((always_inline))
 static inline void int_enable() {
   // Memory constraint is to prevent the compiler
   // from reordering loads/stores with the asm block.
@@ -9,26 +10,31 @@ static inline void int_enable() {
   __asm__ __volatile__("ei" : : : "memory", "cc");
 }
 
-// Disables all interrupts.
+// Disable hardware interrupts.
+__attribute__((always_inline))
 static inline void int_disable() {
   // Same as "ei"
   __asm__ __volatile__("di" : : : "memory", "cc");
 }
 
-// Waits for an interrupt.
+// Wait for a hardware interrupt.
+__attribute__((always_inline))
 static inline void int_wait() {
-  // Fence is to make sure that all writes are not moved past the wait
-  // instruction.
+  // Memory constraint is to make sure that no writes
+  // are moved past the wait instruction.
   __asm__ __volatile__("wait" : : : "memory");
 }
 
-// Triggers a software interrupt with the specified vector.
-// vector must be in the range [0; 63].
+// Trigger a software interrupt with the specified vector.
+// Interrupt vector must be in the range [0; 63].
 #define int_trigger(vector)                                                    \
-  do {                                                                         \
-    _Static_assert((vector) >= 0 && (vector) <= 63,                            \
-                   "Interrupt vector out of range");                           \
+  ({                                                                           \
+    _Static_assert(__builtin_constant_p(vector),                               \
+                   "interrupt vector must be constant");                       \
+    _Static_assert(__builtin_choose_expr(__builtin_constant_p(vector),         \
+                                         (vector) >= 0 && (vector) <= 63, 1),  \
+                   "interrupt vector must be in the range [0; 63]");           \
     __asm__ __volatile__("int %[vec]" : : [vec] "i"(vector) : "memory", "cc"); \
-  } while (0)
+  })
 
 #endif
