27fe4962efc3708468d728d55878ecd8a9575460
aelsi2 <eliseevandrey2005@gmail.com>
2026-05-09 14:01:31 +0700

    Remove CDM LLVM intrinsics and Clang builtins
    No target uses builtins/intrinsics for controlling interrupts.
    They have target-specific headers with macros that expand to inline
    assembly instead. We now have inline assembly properly working, so we
    are doing the same thing.

--- llvm/lib/IR/Intrinsics.cpp
+++ llvm/lib/IR/Intrinsics.cpp
@@ -31,7 +31,6 @@
 #include "llvm/IR/IntrinsicsVE.h"
 #include "llvm/IR/IntrinsicsX86.h"
 #include "llvm/IR/IntrinsicsXCore.h"
-#include "llvm/IR/IntrinsicsCDM.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 
