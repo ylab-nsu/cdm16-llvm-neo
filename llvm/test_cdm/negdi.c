ffd541631ac51f006075ce82820173fea7dd03c1
NetscapeNav <netscapenavigator@yahoo.com>
2026-05-11 21:37:42 +0700

    Add custom lowering for 64-bit arithmetic functions

---
+++ llvm/test_cdm/negdi.c
@@ -0,0 +1,14 @@
+// CHECK reg(r0) 0x0000
+// CHECK reg(r1) 0x0000
+// CHECK reg(r2) 0x0000
+// CHECK reg(r3) 0x0000
+
+__attribute__((noinline))
+long long neg64(long long a) {
+    return -a;
+}
+
+long long main() {
+    volatile long long a = 0x000000000;
+    return neg64(a);
+}
