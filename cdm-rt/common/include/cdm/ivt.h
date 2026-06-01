63ac9544aee90f467883b4e0b6781bf42cab4536
aelsi2 <eliseevandrey2005@gmail.com>
2026-06-01 20:46:37 +0700

    Use 'interrupt' attribute in cdm-rt

--- cdm-rt/common/include/cdm/ivt.h
+++ cdm-rt/common/include/cdm/ivt.h
@@ -2,7 +2,7 @@
 #define _CDM_RT_IVT_H
 
 // Makes the function an interrupt service routine.
-#define ISR __attribute__((CDM_ISR))
+#define ISR __attribute__((interrupt))
 
 // IVT entry.
 typedef struct {
