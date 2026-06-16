d777cdecc4c2e3a61db56e14fa971eecf076b24e
aelsi2 <eliseevandrey2005@gmail.com>
2026-06-17 00:36:07 +0700

    Fix CDM subtarget initialization

--- llvm/lib/Target/CDM/CDMSubtarget.cpp
+++ llvm/lib/Target/CDM/CDMSubtarget.cpp
@@ -3,12 +3,10 @@
 //
 
 #include "CDMSubtarget.h"
-
-// ..MachineFunction
-// ...RegisterInfo
 #include "CDMTargetMachine.h"
 
 using namespace llvm;
+
 #define DEBUG_TYPE "cdm-subtarget"
 
 #define GET_SUBTARGETINFO_TARGET_DESC
@@ -18,10 +16,8 @@ using namespace llvm;
 CDMSubtarget::CDMSubtarget(const Triple &TT, StringRef CPU, StringRef FS,
                            const CDMTargetMachine &TM)
     : CDMGenSubtargetInfo(TT, CPU, CPU, FS),
-      InstrInfo(std::make_unique<CDMInstrInfo>()),
-      FrameLowering(std::make_unique<CDMFrameLowering>(
-          initializeSubtargetDependencies(CPU, FS, TM))),
-      TLInfo(std::make_unique<CDMISelLowering>(TM, *this))
+      FrameLowering(initializeSubtargetDependencies(CPU, FS, TM)), InstrInfo(),
+      TLInfo(TM, *this)
 
 {
   ParseSubtargetFeatures(CPU, /*TuneCPU*/ CPU, FS);
@@ -32,8 +28,4 @@ CDMSubtarget::initializeSubtargetDependencies(StringRef CPU, StringRef FS,
                                               const TargetMachine &TM) {
   ParseSubtargetFeatures(CPU, /*TuneCPU*/ CPU, FS);
   return *this;
-}
-
-const TargetFrameLowering *CDMSubtarget::getFrameLowering() const {
-  return FrameLowering.get();
 }
