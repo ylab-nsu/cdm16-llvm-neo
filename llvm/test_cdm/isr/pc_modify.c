d2271ca02563cea5bc36111ebf272363d7665cb4
aelsi2 <eliseevandrey2005@gmail.com>
2026-08-14 23:56:41 +0700

    Add CDM ISR e2e tests

---
+++ llvm/test_cdm/isr/pc_modify.c
@@ -0,0 +1,37 @@
+// CHECK reg(r0) 7
+
+#include <cdm/ivt.h>
+
+#ifdef __COCAS__
+
+int main() {
+    return 7;
+}
+
+#else
+
+volatile int add_value = 6;
+
+ISR void isr(context_t *context) {
+    context->pc += add_value;
+}
+
+INTERRUPT_VECTORS(
+    VECTOR(isr, 0)
+);
+
+int main() {
+  register int value __asm__("r0");
+  __asm__ volatile("ldi r0, 5\n\t"
+                   "int %[vec]\n\t"
+                   "inc r0\n\t"
+                   "inc r0\n\t"
+                   "inc r0\n\t"
+                   "inc r0\n\t"
+                   "inc r0\n\t"
+                   : "=r"(value)
+                   : [vec] "i"(EXCEPTION_COUNT));
+  return value;
+}
+
+#endif
---
+++ llvm/test_cdm/isr/psr_nomod.c
@@ -0,0 +1,36 @@
+// CHECK reg(r0) -5
+
+#include <cdm/ivt.h>
+
+#ifdef __COCAS__
+
+int main() {
+    return -5;
+}
+
+#else
+
+volatile int and_value = 0xFFFF;
+
+ISR void isr(context_t *context) {
+    context->ps &= and_value;
+}
+
+INTERRUPT_VECTORS(
+    VECTOR(isr, 0)
+);
+
+int main() {
+  register int value __asm__("r0");
+  __asm__ volatile("ldi r0, -5\n\t"
+                   "cmp r0, 0\n\t"
+                   "int %[vec]\n\t"
+                   "blt _main%=\n\t"
+                   "ldi r0, 0x67\n\t"
+                   "_main%=:\n\t"
+                   : "=r"(value)
+                   : [vec] "i"(EXCEPTION_COUNT));
+  return value;
+}
+
+#endif
