0400e88dc4dcc3bcacbf373700903fa31f2c1320
aelsi2 <eliseevandrey2005@gmail.com>
2026-08-27 22:46:50 +0700

    Make the ISR context stack object mutable

--- llvm/lib/Target/CDM/CDMISelLowering.cpp
+++ llvm/lib/Target/CDM/CDMISelLowering.cpp
@@ -147,7 +147,7 @@ SDValue CDMISelLowering::LowerFormalArguments(
     //
     // The GPRs are callee-saved, and CSRs are part of the frame, but PS and PC are not,
     // so the offset only includes the 8 GPRs.
-    int FI = MFI.CreateFixedObject(2 * 10, -2 * 8, true);
+    int FI = MFI.CreateFixedObject(2 * 10, -2 * 8, false);
     SDValue FIPtr = DAG.getFrameIndex(FI, getPointerTy(MF.getDataLayout()));
     InVals.push_back(FIPtr);
     return Chain;
